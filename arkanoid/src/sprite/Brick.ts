109a35aac8bcd197d1774ca1b322430a05124fe1
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2021-03-20 15:19:49 -0600

    Part 5: Brick class (I don't like this)

--- arkanoid/src/sprite/Brick.ts
+++ arkanoid/src/sprite/Brick.ts
@@ -1,3 +1,46 @@
 import { Vector } from '../types';
 
-export class Brick{};
+export class Brick{
+    private brickImage: HTMLImageElement = new Image();
+
+    // I dont get constructors
+    constructor(
+        private brickWidth: number,
+        private brickHeight: number,
+        private brickPosition: Vector,
+        private brickEnergy: number,
+        image: string
+    ){
+        this.brickWidth = brickWidth;
+        this.brickHeight = brickHeight;
+        this.brickPosition = brickPosition;
+        this.brickEnergy = brickEnergy; 
+        this.brickImage.src = image;
+    }
+
+    // Getters why is it done this way?
+    get width(): number {
+        return this.brickWidth;
+    }
+
+    get height(): number {
+        return this.brickHeight;
+    }
+
+    get position(): Vector {
+        return this.brickPosition;
+    }
+
+    get image(): HTMLImageElement {
+        return this.brickImage;
+    }
+
+    get energy(): number {
+        return this.brickEnergy;
+    }
+
+    // Setters
+    set energy(energy: number) {
+        this.brickEnergy = energy;
+    }
+};

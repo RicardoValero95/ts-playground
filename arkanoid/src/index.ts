747dd708faa40ec5b8492c5a4723e95b44656691
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2021-03-20 15:54:13 -0600

    Part 6: Brick helper function

--- arkanoid/src/index.ts
+++ arkanoid/src/index.ts
@@ -1,13 +1,15 @@
 import { CanvasView } from './view/CanvasView';
+
+// Sprites
 import { Ball } from './sprite/Ball';
 import { Brick } from './sprite/Brick';
 import { Paddle } from './sprite/Paddle';
 
+// Images
 import PADDLE_IMAGE from './image/paddle.png'
 import BALL_IMAGE from './image/ball.png'
 
 // Level and colors
-
 import {
     PADDLE_SPEED,
     PADDLE_WIDTH,
@@ -18,6 +20,9 @@ import {
     BALL_STARTX,
     BALL_STARTY
 } from './setup'
+
+// Helpers
+import {createBricks} from './helper'
 
 let gameOver = false;
 let score = 0;
@@ -35,12 +40,23 @@ function setGameWin(view: CanvasView){
 function gameLoop(
     view: CanvasView,
     bricks: Brick[],
-    paddle: Paddle,
-    ball: Ball
-) {}
+    // paddle: Paddle,
+    // ball: Ball
+) {
+    view.clear();
+    view.drawBricks(bricks);
+
+    // I dont like loops 
+    requestAnimationFrame(()=> gameLoop(view, bricks));
+}
 
 function startGame(view: CanvasView){
-
+    // Reset displays
+    score = 0;
+    view.drawInfo('');
+    view.drawScore(score);
+    const bricks = createBricks();
+    gameLoop(view, bricks);
 }
 
 // Create view i dont like passing the id herre 

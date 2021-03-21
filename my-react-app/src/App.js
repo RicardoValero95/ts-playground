52e96a4392f9360a1ec3361395d30e1b21bb2f11
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2021-03-21 15:22:10 -0600

    add uuid and useeffect

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import uuid from 'uuid'
 import TodoList from './TodoList';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, name: 'Todo 1', complete: false}
-  ])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  },[todos])
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos,
+      {id: 1, name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
     <TodoList todos={todos}/>
-    <input type="text" />
-    <button>Add</button>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add</button>
     <button>Clear Complete</button>
     <div>
       left: <span>0</span>

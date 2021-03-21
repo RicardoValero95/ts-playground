a0ba3d61393ce1a9fe27eed147733a7c1c006a13
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2021-03-21 15:12:45 -0600

    step: state and component parameters

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,13 @@
+import React, {useState} from 'react';
 import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name: 'Todo 1', complete: false}
+  ])
   return (
     <>
-    <TodoList />
+    <TodoList todos={todos}/>
     <input type="text" />
     <button>Add</button>
     <button>Clear Complete</button>

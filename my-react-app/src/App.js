aba18d504c6c09d4f2a9a31f45482684f81d73a6
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2021-03-21 15:36:42 -0600

    toggle todo and fix uuid

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react';
-import uuid from 'uuid'
+import { v4 as uuidv4 }  from 'uuid'
 import TodoList from './TodoList';
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
@@ -17,25 +17,32 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   },[todos])
 
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === '') return
     console.log(name)
     setTodos(prevTodos => {
       return [...prevTodos,
-      {id: 1, name: name, complete: false}]
+      {id: uuidv4(), name: name, complete: false}]
     })
     todoNameRef.current.value = null
   }
 
   return (
     <>
-    <TodoList todos={todos}/>
+    <TodoList todos={todos} toggleTodo={toggleTodo}/>
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}>Add</button>
     <button>Clear Complete</button>
     <div>
-      left: <span>0</span>
+      left: <span>{todos.filter(todo => !todo.complete).length}</span>
     </div>
     </>
   );
